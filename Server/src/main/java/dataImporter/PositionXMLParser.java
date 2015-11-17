9bf5b23a976cea1a916a59d27b9c8d7dafce2be9
jframos15 <ramosfrank15@gmail.com>
2015-11-16 21:21:24 -0500

    Base xml parser

--- Server/src/main/java/dataImporter/PositionXMLParser.java
+++ Server/src/main/java/dataImporter/PositionXMLParser.java
@@ -1,20 +1,43 @@
 package dataImporter;
 
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import model.Node ;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 /**
  * Created by EdgarsonRomeus on 15-11-15.
  */
 public class PositionXMLParser {
 
-    ClassLoader classloader = CarJsonParser.class.getClassLoader();
-    File file =  new File(classloader.getResource("PositionMAP.osm").getFile());
+    public static void main(String[] argv){
+        ClassLoader classloader = CarJsonParser.class.getClassLoader();
+        try {
+            File file =  new File(classloader.getResource("PositionMAP.osm").getFile());
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nodes = doc.getElementsByTagName("node");
+            for (int temp = 0; temp < nodes.getLength(); temp++) {
+                Node node = nodes.item(temp);
+                Element element = (Element) node;
+                System.out.println(element.getAttribute("user"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 
 
 
